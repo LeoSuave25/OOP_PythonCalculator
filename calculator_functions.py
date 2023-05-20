6291e8c3c7fbe90cb6dd05fa64d6117977648a28
LeoSuave25 <suaverdez90@gmail.com>
2023-05-20 11:44:33 +0800

    Removed the try except in Division

--- calculator_functions.py
+++ calculator_functions.py
@@ -11,7 +11,4 @@ class Calculator:
         return first_number * second_number
 # Division Method
     def divide(self,first_number,second_number):
-        try:
-            return first_number / second_number
-        except ZeroDivisionError:
-            print("\033[1;31mError: division by zero!\033[0m")
+        return first_number / second_number
