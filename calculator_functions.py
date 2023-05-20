30bdcc844ec8bc639e993abfcdd81966bd1192ac
LeoSuave25 <suaverdez90@gmail.com>
2023-05-20 10:52:51 +0800

    Created class and completed all the 4 basic operation methods

--- calculator_functions.py
+++ calculator_functions.py
@@ -1,5 +1,17 @@
 # Create class
+class Calculator:
 # Add Method
+    def add(self,first_number,second_number):
+        return first_number + second_number
 # Subtract Method
+    def subtract(self,first_number,second_number):
+        return first_number - second_number
 # Multiplication method
-# Division Methof
+    def multiply(self,first_number,second_number):
+        return first_number * second_number
+# Division Method
+    def divide(self,first_number,second_number):
+        try:
+            return first_number / second_number
+        except ZeroDivisionError:
+            print("\033[1;31mError: division by zero!\033[0m")
